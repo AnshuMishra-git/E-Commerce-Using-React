10323f47db72a414246ce0357a32e4c91657470e
Anshu <2202.anshu@gmail.com>
2021-01-08 00:22:28 +0530

    Navbar

---
+++ src/Component/Landing/Landing.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "./Navigation/Navbar";
+
+export default function Landing() {
+  return (
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
+  );
+}
